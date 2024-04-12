781e7e8d4af7b8ffc634c5b0ede5f28ab49afe5a
Gylfi Styrmisson <gylfisstyrmisson@gmail.com>
2024-04-12 23:58:16 +0000

    Setja upp klasa og moppur

--- src/main/java/com/example/throunndaytour/DayTourApplication.java
+++ src/main/java/com/example/throunndaytour/DayTourApplication.java
@@ -1,10 +1,8 @@
 package com.example.throunndaytour;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class DayTourApplication extends Application {
