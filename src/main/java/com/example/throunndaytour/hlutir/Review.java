2f7efc136e961b553c0e2830bd3b40356aa082db
Gylfi Styrmisson <gylfisstyrmisson@gmail.com>
2024-04-13 00:40:09 +0000

    Basic setup fyrir hluti(Booking, DayTour og Review)

--- src/main/java/com/example/throunndaytour/hlutir/Review.java
+++ src/main/java/com/example/throunndaytour/hlutir/Review.java
@@ -1,4 +1,17 @@
 package com.example.throunndaytour.hlutir;
 
 public class Review {
+    private int id;
+    private String title;
+    private String review;
+    private Double rating;
+    private int customerID;
+
+    public Review(int id, String title, String review, Double rating, int customerID) {
+        this.id = id;
+        this.title = title;
+        this.review = review;
+        this.rating = rating;
+        this.customerID = customerID;
+    }
 }
