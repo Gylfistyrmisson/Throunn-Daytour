6fad0b2086938234471b1f41fc1ea62e134a84ed
Gylfi Styrmisson <gylfisstyrmisson@gmail.com>
2024-04-13 15:21:09 +0000

    Basic setup fyrir

--- src/main/java/com/example/throunndaytour/users/Seller.java
+++ src/main/java/com/example/throunndaytour/users/Seller.java
@@ -1,4 +1,7 @@
 package com.example.throunndaytour.users;
 
-public class Seller {
+public class Seller extends User{
+    public Seller(int id, String name, String email, String phonenumber, String kennitala, String password) {
+        super(id, name, email, phonenumber, kennitala, password);
+    }
 }
--- src/main/java/com/example/throunndaytour/users/User.java
+++ src/main/java/com/example/throunndaytour/users/User.java
@@ -1,4 +1,19 @@
 package com.example.throunndaytour.users;
 
-public class User {
+public abstract class User {
+    private int id;
+    private String name = null;
+    private String email;
+    private String phonenumber;
+    private String kennitala;
+    private String password;
+
+    public User(int id, String name, String email, String phonenumber, String kennitala, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.kennitala = kennitala;
+        this.password = password;
+    }
 }
